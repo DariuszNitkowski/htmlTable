bb055e2dd37081ef45f167d70cc4b2a2b875bf9a
darek <dariusz.nitkowski@gmail.com>
2020-04-20 13:18:25 +0200

    sorting data

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-let table, newTable=[]
+// 5. Table should be paginated.
+// 6. App should be responsive (It should work well on both desktop as well as on smaller devices such as mobile phones, tablets).
+// 7. Project should contain full documentation (eg. readme). This point is crucial. You can get extra points for clean docs.
+
+let table=[{id: 82,
+    name: "Boehm - Crist",
+    city: "Weimannhaven",
+    total: 233420,
+    average: 4668,
+    last: 9964},
+    {id: 23,
+        name: "Hirthe, Durgan and Aufderhar",
+        city: "South Randallfort",
+        total: 268764,
+        average: 5375,
+        last: 26563}]
+
+// let table=[]
+let newTable=[]
+let incomes=[]
 let columnClicked=""
 let url="https://recruitment.hal.skygate.io/companies"
-let searchFor={id:"", name:"", city:""}
-let incomes=[]
+let searchFor={id:"", name:"", city:"",lastIncome: "", averageIncome: "", totalIncome: ""}
 let searchButton=document.getElementById("searchButton")
 
 
@@ -45,29 +63,24 @@ async function loopingIncomeData(array) {
 function adjustingData(){
     for (let company of incomes){
         
-        let size=company.incomes.length
-        
         let total=company.incomes.reduce(function(acc,obj){return acc+Number(obj.value)},0)
         let objectTable=table.find(item => item.id === company.id)
         let dates=[]
-        for (item of company.incomes){dates.push(item.date.slice(0,4)+item.date.slice(5,7))}
-
+        for (item of company.incomes)
+            {dates.push(item.date.slice(0,4)+item.date.slice(5,7))}
         let latestMonth=String(Math.max(...dates)).slice(0,4)+"-"+String(Math.max(...dates)).slice(4)
-        
         let score=company.incomes.filter(item=>item.date.slice(0,7)==latestMonth)
         let lastMonthIncome=score.reduce(function(acc,obj){return acc+Number(obj.value)},0)
+        
         objectTable["total"]=Math.round(total)
-        objectTable["average"]=Number(Math.round(total/size))
+        objectTable["average"]=Number(Math.round(total/company.incomes.length))
         objectTable["last"]=Math.round(lastMonthIncome)
     }
 }
 
 
-
-
-
 function load(table, incomes){
-    adjustingData()
+    // adjustingData()
     document.getElementById("data").style.display=""
     document.getElementById("message").innerHTML=""
     const tableBody=document.getElementById("tableData")
@@ -112,7 +125,8 @@ function sortString(columnName, ascending){
 function inputHandle(column){
     let fieldHandle=document.getElementById(column)
     searchFor[column]=fieldHandle.value
-    if (searchFor.id=="" && searchFor.city=="" && searchFor.name=="" && newTable!==table){
+    if (searchFor.id=="" && searchFor.city=="" && searchFor.name=="" && searchFor.lastIncome=="" 
+        && searchFor.totalIncome=="" && searchFor.averageIncome=="" && newTable!==table){
         searchButton.innerHTML="Realod table"    
     }
     else searchButton.innerHTML="Search"
@@ -127,15 +141,28 @@ function search(){
     newTable=newTable.filter(element=>element.name.toLowerCase().includes(searchFor.name.toLowerCase()))}
     if (searchFor.id.length>0){
     newTable=newTable.filter(element=>element.id==searchFor.id)}
+    if (searchFor.lastIncome.length>0 && !isNaN(searchFor.lastIncome)){
+        newTable=newTable.filter(element=>Number(element.last)<=Number(searchFor.lastIncome))}
+    if (searchFor.totalIncome.length>0 && !isNaN(searchFor.totalIncome)){
+        newTable=newTable.filter(element=>element.total<=Number(searchFor.totalIncome))}
+    if (searchFor.averageIncome.length>0 && !isNaN(searchFor.averageIncome)){
+        newTable=newTable.filter(element=>element.average<=Number(searchFor.averageIncome))}
+
+    load(newTable)
     
     document.getElementById("id").value=""
     document.getElementById("city").value=""
     document.getElementById("name").value=""
-    searchFor={id:"", name:"", city:""}
+    document.getElementById("lastIncome").value=""
+    document.getElementById("averageIncome").value=""
+    document.getElementById("totalIncome").value=""
+    searchFor={id:"", name:"", city:"",lastIncome: "", averageIncome: "", totalIncome: ""}
     if (newTable!==table)searchButton.innerHTML="Realod table"
     else searchButton.innerHTML="Search"}
 
 
-fetchingData()
-.then(result=>loopingIncomeData(result))
-.then(()=>load(table, incomes))
+// fetchingData()
+// .then(result=>loopingIncomeData(result))
+// .then(()=>load(table, incomes))
+
+load(table)
